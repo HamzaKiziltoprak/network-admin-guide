795ea418879bfc29b0599cf2e8288b1829b60b06
HamzaKiziltoprak <kiziltoprakh55@gmail.com>
2025-05-17 16:03:36 +0300

    rewrite page and desing

--- project_root/js/main.js
+++ project_root/js/main.js
@@ -1,8 +1,30 @@
+function toggleSidebar() {
+  const sidebar = document.getElementById('sidebar');
+  sidebar.classList.toggle('hidden');
+}
+
+
 function toggleVisibility(id) {
-  const element = document.getElementById(id);
-  if (element.style.display === "block") {
-    element.style.display = "none";
-  } else {
-    element.style.display = "block";
-  }
+  const allSections = [
+    'ip-conflict',
+    'Inter_vlan_communication_problem',
+    'dhcp-issue',
+    'dhcp-spoofing',
+    'default-gateway-issue',
+    'nat-issue',
+    'eigrp-issue',
+    'switch-loop',
+    'gre-issue',
+    'qos-issue'
+  ];
+
+  allSections.forEach(sectionId => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.style.display = (sectionId === id) ? 'block' : 'none';
+      if (sectionId === id && element) {
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }
+  });
 }
